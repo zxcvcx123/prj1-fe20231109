5bb320453935fa731b411ac7bc9941513a493537
JungMo <jmjm0611@naver.com>
2023-11-14 09:51:56 +0900

    로그아웃 기능 추가

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,8 +1,13 @@
 import { Button, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export function NavBar() {
   let navigate = useNavigate();
+
+  function handleLogout() {
+    axios.post("/api/member/logout").then(() => console.log("로그아웃 성공"));
+  }
 
   return (
     <Flex>
@@ -11,6 +16,7 @@ export function NavBar() {
       <Button onClick={() => navigate("/signup")}>가입하기</Button>
       <Button onClick={() => navigate("/member/list")}>회원목록</Button>
       <Button onClick={() => navigate("/login")}>로그인</Button>
+      <Button onClick={handleLogout}>로그아웃</Button>
     </Flex>
   );
 }
